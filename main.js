606912abe6cc1cdf9cb4cce8a63830ce27195d00
Rishikhero <97271798+Rishikhero@users.noreply.github.com>
2022-01-11 09:29:06 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+function si(){
+    document.getElementById("a1").play()
+}
+function si2(){
+    document.getElementById("a2").play()
+}
+function si3(){
+    document.getElementById("a3").play()
+}
